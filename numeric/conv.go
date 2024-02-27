771dd7dbd91e7f29af56e66b2e013154379be89e
Denis Peshkov <denpeshkov98@gmail.com>
2024-02-27 13:13:43 +0400

    add ParseInt

--- numeric/conv.go
+++ numeric/conv.go
@@ -1,12 +1,60 @@
 package numeric
 
-const digits = "0123456789abcdefghijklmnopqrstuvwxyz"
+import (
+	"errors"
+	"math"
+)
 
-// ParseInt interprets a string s in the given base (2 to 36) and returns the corresponding value.
-func ParseInt64(s string, base int) (int64, error) {
-	// TODO
-	return 0, nil
+var ErrRange = errors.New("value out of range")
+var ErrSyntax = errors.New("invalid syntax")
+
+// ParseInt interprets a string s in the given base, for 2 <= base <= 36 and returns the corresponding value.
+// The string may begin with a leading sign: "+" or "-".
+func ParseInt(s string, base int) (int64, error) {
+	if len(s) == 0 {
+		return 0, ErrSyntax
+	}
+
+	var ui uint64
+	isNeg := false
+
+	if s[0] == '-' {
+		isNeg = true
+		s = s[1:]
+	} else if s[0] == '+' {
+		s = s[1:]
+	}
+
+	var d byte
+	// conversion optimized and no allocations
+	for _, c := range []byte(s) {
+		switch {
+		case c >= '0' && c <= '9':
+			d = c - '0'
+		case lower(c) >= 'a' && lower(c) <= 'z':
+			d = lower(c) - 'a' + 10
+		default:
+			return 0, ErrSyntax
+		}
+		ui = ui*uint64(base) + uint64(d)
+	}
+	if !isNeg {
+		if ui > math.MaxInt64 {
+			return math.MaxInt64, ErrRange
+		}
+		return int64(ui), nil
+	}
+	if ui > -math.MinInt64 {
+		return math.MinInt64, ErrRange
+	}
+	return -int64(ui), nil
 }
+
+func lower(c byte) byte {
+	return c | ('a' - 'A')
+}
+
+const digits = "0123456789abcdefghijklmnopqrstuvwxyz"
 
 // FormatInt returns the string representation of i in the given base, for 2 <= base <= 36.
 // The result uses the lower-case letters 'a' to 'z' for digit values >= 10.
--- numeric/conv_test.go
+++ numeric/conv_test.go
@@ -1,10 +1,120 @@
 // Based on https://github.com/golang/go/blob/master/src/strconv/itoa_test.go
+// Based on https://github.com/golang/go/blob/master/src/strconv/atoi_test.go
 
 package numeric
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 )
+
+type parseIntTest struct {
+	in   string
+	base int
+	out  int64
+	err  error
+}
+
+var parseIntTests = []parseIntTest{
+	{"", 10, 0, ErrSyntax},
+	{"0", 10, 0, nil},
+	{"-0", 10, 0, nil},
+	{"+0", 10, 0, nil},
+	{"1", 10, 1, nil},
+	{"-1", 10, -1, nil},
+	{"+1", 10, 1, nil},
+	{"12345", 10, 12345, nil},
+	{"-12345", 10, -12345, nil},
+	{"012345", 10, 12345, nil},
+	{"-012345", 10, -12345, nil},
+	{"98765432100", 10, 98765432100, nil},
+	{"-98765432100", 10, -98765432100, nil},
+	{"9223372036854775807", 10, 1<<63 - 1, nil},
+	{"-9223372036854775807", 10, -(1<<63 - 1), nil},
+	{"9223372036854775808", 10, 1<<63 - 1, ErrRange},
+	{"-9223372036854775808", 10, -1 << 63, nil},
+	{"9223372036854775809", 10, 1<<63 - 1, ErrRange},
+	{"-9223372036854775809", 10, -1 << 63, ErrRange},
+	{"-1_2_3_4_5", 10, 0, ErrSyntax}, // base=10 so no underscores allowed
+	{"-_12345", 10, 0, ErrSyntax},
+	{"_12345", 10, 0, ErrSyntax},
+	{"1__2345", 10, 0, ErrSyntax},
+	{"12345_", 10, 0, ErrSyntax},
+	{"123%45", 10, 0, ErrSyntax},
+	{"012345", 10, 12345, nil},
+	{"000000000012345", 10, 12345, nil},
+
+	// other bases
+	{"g", 17, 16, nil},
+	{"10", 25, 25, nil},
+	{"holycow", 35, (((((17*35+24)*35+21)*35+34)*35+12)*35+24)*35 + 32, nil},
+	{"holycow", 36, (((((17*36+24)*36+21)*36+34)*36+12)*36+24)*36 + 32, nil},
+
+	// base 2
+	{"0", 2, 0, nil},
+	{"-1", 2, -1, nil},
+	{"1010", 2, 10, nil},
+	{"1000000000000000", 2, 1 << 15, nil},
+	{"111111111111111111111111111111111111111111111111111111111111111", 2, 1<<63 - 1, nil},
+	{"1000000000000000000000000000000000000000000000000000000000000000", 2, 1<<63 - 1, ErrRange},
+	{"-1000000000000000000000000000000000000000000000000000000000000000", 2, -1 << 63, nil},
+	{"-1000000000000000000000000000000000000000000000000000000000000001", 2, -1 << 63, ErrRange},
+
+	// base 8
+	{"-10", 8, -8, nil},
+	{"57635436545", 8, 057635436545, nil},
+	{"100000000", 8, 1 << 24, nil},
+
+	// base 16
+	{"10", 16, 16, nil},
+	{"-123456789abcdef", 16, -0x123456789abcdef, nil},
+	{"7fffffffffffffff", 16, 1<<63 - 1, nil},
+}
+
+func TestParseInt(t *testing.T) {
+	for i := range parseIntTests {
+		test := &parseIntTests[i]
+		out, err := ParseInt(test.in, test.base)
+		if test.out != out || !errors.Is(test.err, err) {
+			t.Errorf("ParseInt(%q, %v, 64) = %v, %v; want %v, %v",
+				test.in, test.base, out, err, test.out, test.err)
+		}
+	}
+}
+
+func BenchmarkParseInt(b *testing.B) {
+	b.Run("Pos", func(b *testing.B) {
+		benchmarkParseInt(b, 1)
+	})
+	b.Run("Neg", func(b *testing.B) {
+		benchmarkParseInt(b, -1)
+	})
+}
+
+type benchCase struct {
+	name string
+	num  int64
+}
+
+func benchmarkParseInt(b *testing.B, neg int) {
+	cases := []benchCase{
+		{"7bit", 1<<7 - 1},
+		{"26bit", 1<<26 - 1},
+		{"31bit", 1<<31 - 1},
+		{"56bit", 1<<56 - 1},
+		{"63bit", 1<<63 - 1},
+	}
+	for _, cs := range cases {
+		b.Run(cs.name, func(b *testing.B) {
+			s := fmt.Sprintf("%d", cs.num*int64(neg))
+			for i := 0; i < b.N; i++ {
+				out, _ := ParseInt(s, 10)
+				BenchSink += int(out)
+			}
+		})
+	}
+}
 
 type itob64Test struct {
 	in   int64
@@ -60,7 +170,7 @@ func TestFormatInt(t *testing.T) {
 	for _, test := range itob64tests {
 		s := FormatInt(test.in, test.base)
 		if s != test.out {
-			t.Errorf("FormatInt(%v, %v) = %v want %v", test.in, test.base, s, test.out)
+			t.Errorf("FormatInt(%v, %v) = %v; want %v", test.in, test.base, s, test.out)
 		}
 	}
 }
